ff1e1c1572dd9d0cc9eb20e57595819e5fae4f0e
Mahmud Al Hakim <Mahmud.alhakim@gmail.com>
2019-10-18 10:49:42 +0200

    First Commit

--- src/Test.java
+++ src/Test.java
@@ -14,38 +14,5 @@ public class Test {
     public static void main(String[] args) {
 
 
-    // 1. Läs in ett tal via en dial
-        // TODO: 2019-10-18
-        // Vi startar med testkod t.ex. talet 11
-        // 1. Läs in ett tal via en dialogruta
-        String input = JOptionPane.showInputDialog("Ange ett heltal");
-        if(input == null)
-            System.exit(0);
-
-    int number = Integer.parseInt(input);
-
-    // 2. Undersök om talet är ett primtal
-    boolean isPrime = true;
-    // Upprepa följande för alla tal n i intervallet 2 till talet-1
-        for (int n = 2 ; n < number ; n++) {
-            // Om resten blir 0 när man dividerar talet med n
-            if(number % n == 0){
-                // Så sätt isPrime till false
-                isPrime = false;
-            }
-        }
-
-        System.out.println(number + " " + isPrime);
-
-    // 3. Visa resultatet
-//    if(isPrime)
-//        System.out.println(number + " är primtal!");
-//    else
-//        System.out.println(number + " är inte primtal!");
-
-
-
-
-
     }
 }
