6a902a056bbb4cdc1a1a30fd984387b6ff30a5a5
Mahmud Al Hakim <Mahmud.alhakim@gmail.com>
2019-10-18 10:44:36 +0200

    First Commit

---
+++ Lektion_02/src/Test.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Mahmud Al Hakim
+ * Nackademin - Stockholm - Sweden
+ * Project: OOP_HT19
+ * Date: 2019-10-18 10:44
+ * Copyright: MIT
+ */
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("TEST");
+    }
+}
