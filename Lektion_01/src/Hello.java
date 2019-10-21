5e13c479284935e2b8cc566470ff8e8bc1682a55
Mahmud Al Hakim <Mahmud.alhakim@gmail.com>
2019-10-21 14:38:37 +0200

    Lektion 1

---
+++ Lektion_01/src/Hello.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+/**
+ * Created by Mahmud Al Hakim
+ * Nackademin - Stockholm - Sweden
+ * Project: OOP_HT19
+ * Date: 2019-10-21 14:10
+ * Copyright: MIT
+ */
+
+public class Hello {
+    public static void main(String[] args) {
+        // Visa Hello World i terminalen
+        System.out.println("Hello World!");
+
+        // Visa Hello World i en dialogruta
+        JOptionPane.showMessageDialog(null, "Hello World");
+    }
+}
