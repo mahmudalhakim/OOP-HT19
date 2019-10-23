867c354e8e4fc718ec7a6cdadce5a6c5152c6be5
Mahmud Al Hakim <Mahmud.alhakim@gmail.com>
2019-10-23 08:31:28 +0200

    Lektion 2

---
+++ Lektion_02/src/Test.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Mahmud Al Hakim
+ * Nackademin - Stockholm - Sweden
+ * Project: OOP_HT19
+ * Date: 2019-10-23 08:30
+ * Copyright: MIT
+ */
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Test från Lektion 2");
+    }
+}
