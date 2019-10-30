818d3bee8afa51c7be7433d01c1161c14b016145
Mahmud Al Hakim <Mahmud.alhakim@gmail.com>
2019-10-30 11:40:09 +0100

    Lektion 5

---
+++ Lektion_05/src/FizzBuzz.java
@@ -0,0 +1,47 @@
+/**
+ * Created by Mahmud Al Hakim
+ * Nackademin - Stockholm - Sweden
+ * Project: OOP_HT19
+ * Date: 2019-10-30 11:24
+ * Copyright: MIT
+ */
+
+public class FizzBuzz {
+
+    /***********************************************************
+     * Skapa ett program som testar om ett tal är jämnt delbart med 3.
+     * I så fall skriver programmet ut "Fizz".
+     * Om talet är jämnt delbart med 5 skrivs "Buzz" ut.
+     * Slutligen skrivs "FizzBuzz" ut om talet är jämnt delbart med både 3 och 5.
+     * Testa talen från 1 till 100.
+     * Här till höger visas 1 till 15.
+     */
+
+    public static void main(String[] args) {
+
+        // Testkod
+        int number = 15;
+
+        // Visa alla tal från 1 till 100
+        for (number = 1; number <= 100; number++) {
+
+            System.out.print(number + ": ");
+
+            // Testa delbarhet med 3 och 5
+            if ((number % 3 == 0) && (number % 5 == 0))
+                System.out.println("FizzBuzz");
+
+            // Testa delbarhet med 3
+            else if (number % 3 == 0)
+                System.out.println("Fizz");
+
+            // Testa delbarhet med 5
+            else if (number % 5 == 0)
+                System.out.println("Buzz");
+
+            // Visa number om det är varken delbart med 3 eller 5
+            else
+                System.out.println(number);
+        }
+    }
+}
