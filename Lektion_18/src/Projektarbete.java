7ef117723da85f6d4a2635a66bb6dccf92ba0142
Mahmud Al Hakim <Mahmud.alhakim@gmail.com>
2019-11-28 12:41:36 +0100

    Lektion 18

--- Lektion_18/src/Projektarbete.java
+++ Lektion_18/src/Projektarbete.java
@@ -32,29 +32,39 @@ public class Projektarbete {
     Här finns många exempel på olika kortspel
     https://sv.wikipedia.org/wiki/Lista_%C3%B6ver_kortspel
 
-    3. Matematik
-    3.1. Utveckla miniräknaren (uppgift 1) så att den kan beräkna flera "saker".
-    3.3. Skapa ett program som beräknar area och omkrets för olika figurer (rektanglar, cirklar, triangar m.m.)
+    3. Tre i rad
+       Skapa spelet "Tre i rad " som körs i terminalen
+       https://sv.wikipedia.org/wiki/Tre_i_rad
 
-    4. BMI
+    4. Matematik
+    4.1. Utveckla miniräknaren (uppgift 1) så att den kan beräkna flera "saker".
+    4.2. Skapa ett program som beräknar area och omkrets för olika figurer (rektanglar, cirklar, triangar m.m.)
+
+    5. BMI
     Utveckla uppgiften om BMI genom att t.ex.
     ge förslag på hur många kilo måste man gå ner för att hamna i viktklassen "Normal".
     Du kan också låta flera personer mata in längd och vikt
     och göra lite analys över deras vikter, längder och BMI.
 
-    5. Analyser
-    5.1. Utveckla Skansholm exempel om väderobservationer på sidan 292
+    6. Analyser
+    6.1. Utveckla Skansholm exempel om väderobservationer på sidan 292
          Ett program som läser in temperatur och vindhastighet,
          för olika städer under olika tidpunkter från en textfil.
          Programmet ska analysera data och visa en sammanställning på resultatet.
 
-    5.2 Ett program som läser in betyg för olika studenter i olika kurser
+    6.2 Ett program som läser in betyg för olika studenter i olika kurser
         och visar sammanställning på resultatet.
         Antal IG, G, VG i olika kurser.
 
-    6. Bank
+    7. Bank
        Ett program som skapar olika bankkonton åt olika personer.
        En person skall kunna sätt in och ta ut pengar från sitt eget konto
+
+    8. Bilförsäljning
+       Skapa ett program som hanterar försäljning av bilar.
+       En person skall kunna köpa flera bilar.
+       En såld bil kan inte säljas igen!
+
 
      */
 
